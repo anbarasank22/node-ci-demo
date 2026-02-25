df32bfd672e710387dda392d6e0a7cea941469b0
Anbu K <anbarasank22@gmail.com>
2026-02-25 21:09:55 +0530

    updated index file

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
+const http = require("http");
+
 function sum(a, b) {
   return a + b;
 }
 
-module.exports = sum;
+const server = http.createServer((req, res) => {
+  const result = sum(5, 10);
+  res.write("Sum is: " + result);
+  res.end();
+});
 
+server.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
